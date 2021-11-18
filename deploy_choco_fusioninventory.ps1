9a6d5f743c93ce9bb391c5d25aa93a386b98053b
Stephen Davis <68449281+nativeit@users.noreply.github.com>
2021-11-18 02:51:08 -0500

    Updated with variables and fixed issue with local directory.

--- deploy_choco_fusioninventory.ps1
+++ deploy_choco_fusioninventory.ps1
@@ -1,25 +1,25 @@
 # |  #   #   #   #   #   #   #   #   #   #   #   #   #   __//    N A T I V E . I T    //__    #   #   #   #   #   #   #   #   #   #   #   |
 # |                                                                                                                                       |
-# |  This script checks for Chocolatey and installs it if needed. It then installs FusionInventory for our GPLI server at                 |
-# |  psa.nativeit.net (BE SURE TO CHANGE THIS IF YOU FORK THIS). A local report directory is added at C:\Support\FusionInventory.         |
+# |  This script checks for Chocolatey and installs it if needed. It then installs FusionInventory for a GPLI server                      |
+# |  specified by [{fusion_server_uri}]. A local report directory is added at C:\Support\FusionInventory.                                 |
 # |                                                                                                                                       |
 # |  This script was revised, tested, and approved on 2021-11-18.                                                                         |
 # |                                                                                                                                       |
 # |  #   #   #   #   #   #   #   #   #   #   #   #   #   __//    desk.nativeit.net     //__   #   #   #   #   #   #   #   #   #   #   #   |
 
-# 
 # Must be run as admin/system
 
 # Create directory for Fusion Inventory reports
 mkdir C:\Support\FusionInventory
 
-# Adjust permissions on C:\Support\FusionInventory
-$mypath = "C:\Support\FusionInventory"
-$myacl = Get-Acl $mypath
-$myaclentry = "Everyone","FullControl","Allow"
-$myaccessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($myaclentry)
-$myacl.SetAccessRule($myaccessrule)
-Get-ChildItem -Path "$mypath" -Recurse -Force | Set-Acl -AclObject $myacl -Verbose
+# Set install params and adjust permissions on C:\Support\FusionInventory
+$fi_server = [{fusion_server_uri}] # ie: https://glpi.hostname.com/plugins/fusioninventory/ or https://hostname.com/glpi/plugins/fusioninventory/
+$localdir = "C:\Support\FusionInventory"
+$local_acl = Get-Acl $localdir
+$local_aclentry = "Everyone","FullControl","Allow"
+$acl_accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($local_aclentry)
+$local_acl.SetAccessRule($acl_accessrule)
+Get-ChildItem -Path "$localdir" -Recurse -Force | Set-Acl -AclObject $local_acl -Verbose
 
 # Be sure choco.exe is added to $PATH
 $choco = Join-Path -Path $env:ProgramData -ChildPath "chocolatey\choco.exe"
@@ -32,4 +32,4 @@ if (!(Test-Path $choco)) {
 }
 
 # Install Fusion Inventory agent with arguments and [{tag}]
-choco install fusioninventory-agent --yes --no-progress --installargs "/execmode=service /delaytime=3600 /server='https://psa.nativeit.net/plugins/fusioninventory/' /local="C:\Support\FusionInventory" /debug=1 /no-start-menu /tag={[computer_tag]} /add-firewall-exception /runnow"
+choco install fusioninventory-agent --yes --no-progress --installargs "/execmode=service /delaytime=3600 /server='$fi_server' /local=$localdir /debug=1 /no-start-menu /tag='deploy-test' /add-firewall-exception /runnow"
